06fe56794c50203544bbd547879673fe84cc6398
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-09-16 19:37:27 +0530

    Error Handling done to display the Tasks Dashboard

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -3,6 +3,8 @@ import { getTasks } from "../utils/taskSlice";
 import AddTask from "./AddTask";
 import TaskStatusWise from "./TaskStatusWise";
 import { useDispatch, useSelector } from "react-redux";
+import Loading from "./Loading";
+import Failed from "./Failed";
 const TaskBoard = () => {
   const dispatch = useDispatch();
   const { tasks, error, status, successMessage } = useSelector(
@@ -20,8 +22,14 @@ const TaskBoard = () => {
   return (
     <>
       <main className=" w-full min-h-screen relative">
-        <AddTask />
-        <TaskStatusWise />
+        {status === "loading" && <Loading />}
+        {status === "failed" && <Failed />}
+        {status === "succeeded" && (
+          <>
+            <AddTask />
+            <TaskStatusWise tasks={tasks} />
+          </>
+        )}
       </main>
     </>
   );
