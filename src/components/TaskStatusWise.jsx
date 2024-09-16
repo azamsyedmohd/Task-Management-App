ab9264b99cd0c8f3b22b4b30c589201964b56545
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-09-16 22:03:49 +0530

    Tasks dividide on the basis of statuses

--- src/components/TaskStatusWise.jsx
+++ src/components/TaskStatusWise.jsx
@@ -1,14 +1,22 @@
+/* eslint-disable react/prop-types */
 import TodoTasks from "./TodoTasks";
 import InProgressTasks from "./InProgressTasks";
 import CompletedTasks from "./CompletedTasks";
 // eslint-disable-next-line react/prop-types
 const TaskStatusWise = ({ tasks }) => {
+  const completedTasks = tasks?.map(
+    (task) => !task?.inProgress && task?.completed
+  );
+  const todosTasks = tasks?.map(
+    (task) => !task?.inProgress && !task?.completed
+  );
+  const progressTasks = tasks?.map((task) => task?.inProgress);
   return (
     <>
       <section className="w-full px-10 py-2 flex flex-col gap-6 lg:flex-row ">
-        <TodoTasks tasks={tasks} />
-        <InProgressTasks tasks={tasks} />
-        <CompletedTasks tasks={tasks} />
+        <TodoTasks tasks={todosTasks} />
+        <InProgressTasks tasks={progressTasks} />
+        <CompletedTasks tasks={completedTasks} />
       </section>
     </>
   );
